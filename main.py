a70d1deaf6147933e6ec152b313b1b0e10052ad4
SergantDornan <microsoftvis@gmail.com>
2024-10-29 13:25:31 +0300

    with docs

--- main.py
+++ main.py
@@ -1,110 +1,94 @@
 from operator import itemgetter
 
-class Microprocessor:
- """Microprocessor"""
- def __init__(self, id, model, clock_speed, computer_id):
-  self.id = id
-  self.model = model
-  self.clock_speed = clock_speed
-  self.computer_id = computer_id
+class Schoolboy:
+  """Schoolboy"""
+  def __init__(self, id, surname, mark, class_id):
+    self.id = id
+    self.surname = surname
+    self.mark = mark
+    self.class_id = class_id
 
-class Computer:
- """Computer"""
- def __init__(self, id, name):
-  self.id = id
-  self.name = name
+class Class:
+  """Class"""
+  def __init__(self, id, name):
+    self.id = id
+    self.name = name
 
-class MicroprocessorsComputer:
- """
- 'Microprocessors of a Computer' for implementing
- many-to-many relationship
- """
- def __init__(self, computer_id, microprocessor_id):
-  self.computer_id = computer_id
-  self.microprocessor_id = microprocessor_id
+class SchoolboyClass:
+  """
+  'Schoolboys of a Class' for implementing
+  many-to-many relationship
+  """
+  def __init__(self, class_id, schoolboy_id):
+    self.class_id = class_id
+    self.schoolboy_id = schoolboy_id
 
-# Computers
-computers = [
- Computer(1, 'Computer 1'),
- Computer(2, 'Computer 2'),
- Computer(3, 'Computer 3'),
+classes = [
+  Class(1, '9A'),
+  Class(2, '10Б'),
+  Class(3, '11В'),
+  Class(4, '9Г'),
+  Class(5, '10Д'),
 ]
 
-# Microprocessors
-microprocessors = [
- Microprocessor(1, 'Intel Core i7-12700K', 5.0, 1),
- Microprocessor(2, 'AMD Ryzen 9 5950X', 4.9, 2),
- Microprocessor(3, 'Intel Core i5-12600K', 4.9, 3),
- Microprocessor(4, 'AMD Ryzen 7 5800X', 4.7, 1),
- Microprocessor(5, 'Intel Core i3-12100', 4.3, 2),
+schoolboys = [
+  Schoolboy(1, 'Иванов', 5, 1),
+  Schoolboy(2, 'Петров', 4, 2),
+  Schoolboy(3, 'Сидоров', 3, 3),
+  Schoolboy(4, 'Кузнецов', 5, 4),
+  Schoolboy(5, 'Смирнов', 4, 5),
 ]
 
-microprocessors_computers = [
- MicroprocessorsComputer(1, 1),
- MicroprocessorsComputer(2, 2),
- MicroprocessorsComputer(3, 3),
- MicroprocessorsComputer(1, 4),
- MicroprocessorsComputer(2, 5),
+schoolboys_classes = [
+  SchoolboyClass(1, 1),
+  SchoolboyClass(2, 2),
+  SchoolboyClass(3, 3),
+  SchoolboyClass(4, 4),
+  SchoolboyClass(5, 5),
+  SchoolboyClass(1, 2),
+  SchoolboyClass(2, 3),
+  SchoolboyClass(3, 4),
+  SchoolboyClass(4, 5),
+  SchoolboyClass(5, 1),
 ]
 
 def main():
- """Main function"""
+  """Main function"""
 
- # One-to-many data connection 
- one_to_many = [(m.model, m.clock_speed, c.name) 
-  for c in computers 
-  for m in microprocessors 
-  if m.computer_id==c.id]
- 
- # Many-to-many data connection
- many_to_many_temp = [(c.name, mc.computer_id, mc.microprocessor_id) 
-  for c in computers 
-  for mc in microprocessors_computers 
-  if c.id==mc.computer_id]
- 
- many_to_many = [(m.model, m.clock_speed, c_name) 
-  for c_name, c_id, m_id in many_to_many_temp
-  for m in microprocessors if m.id==m_id]
+  one_to_many = [(s.surname, s.mark, c.name) 
+    for c in classes 
+    for s in schoolboys 
+    if s.class_id == c.id]
 
- print('Task G1')
- res_11 = [ (c.name, [m.model for m in microprocessors if m.computer_id == c.id]) for c in computers if c.name[0] == 'C'] 
- print(res_11)
- 
- print('\nTask G2')
- res_12_unsorted = []
- # Iterate over all computers
- for c in computers:
-  # List of computer microprocessors
-  c_mps = list(filter(lambda i: i[2]==c.name, one_to_many))
-  # If computer is not empty  
-  if len(c_mps) > 0:
-   # Clock speeds of the computer's microprocessors
-   c_freqs = [freq for _,freq,_ in c_mps]
-   # Maximum clock speed of the computer's microprocessors
-   c_max_freq = max(c_freqs)
-   res_12_unsorted.append((c.name, c_max_freq))
+  many_to_many_temp = [(c.name, sc.class_id, sc.schoolboy_id) 
+    for c in classes 
+    for sc in schoolboys_classes 
+    if c.id == sc.class_id]
 
+  many_to_many = [(s.surname, s.mark, c_name) 
+    for c_name, c_id, s_id in many_to_many_temp
+    for s in schoolboys if s.id == s_id]
 
- # Sorting by maximum clock speed
- res_12 = sorted(res_12_unsorted, key=lambda item: item[1], reverse=True)
- print(res_12)
+  print('Task B1')
+  res_11 = [ (s.surname, c.name) for c in classes for s in schoolboys if s.class_id == c.id and s.surname.endswith('ов')] 
+  print(res_11)
+  
+  print('\nTask B2')
+  res_12_unsorted = []
+  for c in classes:
+    c_schoolboys = list(filter(lambda i: i[2] == c.name, one_to_many))
+    if len(c_schoolboys) > 0:
+      c_marks = [mark for _, mark, _ in c_schoolboys]
+      c_avg_mark = sum(c_marks) / len(c_marks)
+      res_12_unsorted.append((c.name, c_avg_mark))
 
+  res_12 = sorted(res_12_unsorted, key=itemgetter(1))
+  print(res_12)
 
- print('\nTask G3')
- res_13 = {}
- # Iterate over all computers
- for c in computers:
-  # List of computer microprocessors
-  c_mps = list(filter(lambda i: i[2]==c.name, many_to_many))
-  # Only microprocessor models
-  c_mps_models = [x for x,_,_ in c_mps]
-  # Add the result to the dictionary
-  # key - computer, value - list of microprocessor models
-  res_13[c.name] = c_mps_models
-
-
- print(res_13)
-
+  print('\nTask B3')
+  res_13 = sorted(many_to_many, key=itemgetter(0)) # Sort by schoolboy surname
+  print(res_13)
 
 if __name__ == '__main__':
- main()
+  main()
+
